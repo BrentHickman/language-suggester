6d9034fce3e14d63f0ce2b033f586d778129fc95
BrentHickman <brenthickman@ymail.com>
2023-01-13 15:21:18 -0800

    new bridging logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,12 @@ function handleSubmission(event) {
   const answerQ01 = document.querySelector("input[name='q01']:checked").value;
   const answerQ02 = document.querySelector("input[name='q02']:checked").value;
   const answerQ03 = document.querySelector("input[name='q03']:checked").value;
+  const answerQ04 = document.querySelector("input[name='q04']:checked").value;
+
+  if (answerQ01 === "null" || answerQ02 === "null"  || answerQ03 === "null" || answerQ04 === "null"){
+  error.removeAttribute("class");
+  }
+  
 
   let javascript = document.getElementById("javascript");
   let python = document.getElementById("python");
@@ -17,34 +23,31 @@ function handleSubmission(event) {
   //   document.getElementById("error").removeAttribute("class");
   // }
 
-  if(answerQ01 === "none" && answerQ02 === ("app" || "game") && answerQ03 === ("days" || "weeks")){
+  if(answerQ01 === "none" && answerQ02 === "app" || "game" && answerQ03 === "weeks"){
     javascript.removeAttribute("class");
   }
     
-  else if("some" === answerQ01){
+  else if(answerQ01 ==="none" && answerQ02 === "data" && answerQ03 === "weeks"){
     python.removeAttribute("class");
   }
 
-  else if("some" === answerQ01){
+  else if(answerQ01="some" && answerQ02 === "game" || "app"){
     cSharp.removeAttribute("class");
   }
 
-  else if("some" === answerQ01){
+  else if("alot" === answerQ01 && answerQ02 === "app" || "game"){
     ruby.removeAttribute("class");
   }
   
-  else if("some" === answerQ01){
+  else if("some" === answerQ01 && answerQ02 === "app"){
     swift.removeAttribute("class");
   }
   
-  else if("alot" === answerQ01){
+  else if(answerQ02 === "data" && answerQ03 === "months"){
     rust.removeAttribute("class");
   }
 
-  // else {
-  //   error.removeAttribute("class");
-  //
-  
+
 }
 window.addEventListener("load", function() {
   const form = document.getElementById("survey");
