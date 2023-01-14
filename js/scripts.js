f8a0bd44cfa3f2d01b853b2e8e27be902cb925a0
BrentHickman <brenthickman@ymail.com>
2023-01-13 16:26:29 -0800

    debugging reset function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // User Interface Logic
+
 function handleSubmission(event) {
   event.preventDefault();
-
   const answerQ01 = document.querySelector("input[name='q01']:checked").value;
   const answerQ02 = document.querySelector("input[name='q02']:checked").value;
   const answerQ03 = document.querySelector("input[name='q03']:checked").value;
@@ -39,9 +39,16 @@ function handleSubmission(event) {
     rust.removeAttribute("class");
   }
 
+}
+
+function resetSubmission(event) {
+  event.preventDefault(reset);
 
 }
 window.addEventListener("load", function() {
   const form = document.getElementById("survey");
   form.addEventListener("submit", handleSubmission);
+  form.removeEventListener("submit", resetSubmission);
 });
+
+
