48099553e49dfae29e8572fcd24f31cc2f05a12f
BrentHickman <brenthickman@ymail.com>
2023-01-22 09:41:08 -0800

    Previous results clear on subsequent submission now, reset button has been phased out

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-// User Interface Logic
+// Business Logic
+let survey = document.querySelector("#survey");
 
 function handleSubmission(event) {
   event.preventDefault();
@@ -13,46 +14,48 @@ function handleSubmission(event) {
   let ruby = document.getElementById("ruby");
   let swift = document.getElementById("swift");
   let rust = document.getElementById("rust");
-  
+  let error = document.getElementById("error");
 
   if(answerQ01 === "none" && answerQ02 === "app" || answerQ01 === "none" && answerQ02 === "game"){
     javascript.removeAttribute("class");
-  }
-    
+  } 
   else if(answerQ01 === "none" && answerQ02 === "data"){
     python.removeAttribute("class");
   }
-
   else if(answerQ01 === "some" && answerQ02 === "game" || answerQ01 ==="some" && answerQ02 ==="app"){
     cSharp.removeAttribute("class");
   }
-
   else if(answerQ01=== "alot" && answerQ02 === "game"){
     ruby.removeAttribute("class");
   }
-  
   else if("alot" === answerQ01 && answerQ02 === "app"){
     swift.removeAttribute("class");
   }
-  
   else if(answerQ01 !== "none" && answerQ02 === "data"){
     rust.removeAttribute("class");
   }
-
+  else {
+    error.removeAttribute("class");
+  }
+  const form = document.getElementById("survey");
+  form.addEventListener("submit", resetAfterSubmit);
 };
 
+// User Interface Logic
 window.addEventListener("load", function() {
   const form = document.getElementById("survey");
   form.addEventListener("submit", handleSubmission);
-
 });
 
-///reset button
-window.addEventListener("reset", function() {
+///reset results after submit
+function resetAfterSubmit(event) {
+  event.preventDefault();
   document.getElementById("javascript").classList.add("hidden");
   document.getElementById("python").classList.add("hidden");
   document.getElementById("cSharp").classList.add("hidden");
   document.getElementById("ruby").classList.add("hidden");
   document.getElementById("swift").classList.add("hidden");
   document.getElementById("rust").classList.add("hidden");
-});
+  document.getElementById("error").classList.add("hidden");
+  return handleSubmission(event);
+};
